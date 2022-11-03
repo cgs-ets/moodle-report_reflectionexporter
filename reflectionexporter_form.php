6587887e82f1792e93f10eb7aa50effd616966f5
Veronica Bermegui <verisb@gmail.com>
2022-11-03 15:56:11 +1100

    Style fixes and control when there are no groups available

--- reflectionexporter_form.php
+++ reflectionexporter_form.php
@@ -56,14 +56,15 @@ class reflectionexporter_form extends moodleform {
         $mform->addHelpButton('attachment_filemanager', 'attachment_filemanager', 'report_reflectionexporter');
         $mform->addRule('attachment_filemanager', null, 'required');
 
-        /**
-         * Ask if the user is going to generate the reflections for their student OR do it on behalf of others.
-         * If its on behalf of others, open the option to select the groups --> Each group will have the teacher 
-         * responsible for the students in the group reflection.
-         */
-        $mform->addElement('checkbox', 'onbehalf', get_string('onbehalf', 'report_reflectionexporter'));
+        // Ask if the user is going to generate the reflections for their student OR do it on behalf of others.
+        // If its on behalf of others
+        // Open the option to select the groups --> Each group will have the teacher responsible for the students in the group reflection.
 
+        $mform->addElement('checkbox', 'onbehalf', get_string('onbehalf', 'report_reflectionexporter'));
+        $mform->disabledIf('onbehalf','groupscount', 'eq', 0);
+        $mform->addHelpButton('onbehalf', 'onbehalf', 'report_reflectionexporter');
         // Supervisor initials.  DEFAULT. Teacher is doing their job only.
+
         $attributes = array('size' => '6');
         $mform->addElement('text', 'supervisorinitials', get_string('supervisorinitials', 'report_reflectionexporter'), $attributes);
         $mform->settype('supervisorinitials', PARAM_TEXT);
@@ -73,6 +74,9 @@ class reflectionexporter_form extends moodleform {
 
         // Group selection.
         $groups = reflectionexportermanager::get_groups_with_teachers($this->_customdata['id']);
+        $mform->addElement('text', 'groupscount', 'Count',['class' => 'reflection-exporter-group-count']);
+        $mform->setType('groupscount', PARAM_RAW);
+        $mform->setDefault('groupscount', count($groups));
 
         $grouparray = array();
         $grouparrayaux = array();
@@ -85,30 +89,36 @@ class reflectionexporter_form extends moodleform {
             $data = new stdClass();
             $data->groupname = $group->name;
             $data->groupid = $groupid;
-            $data->courseid =  $this->_customdata['id'];
+            $data->courseid = $this->_customdata['id'];
             $data->teachers = $group->teachers;
             $data->students = $group->students;
             $grouparrayaux[] = $data;
         }
-        $select =  $mform->addElement('select', 'coursegroups', get_string('coursegroups', 'report_reflectionexporter'), $grouparray);
-        $select->setMultiple(true);
-        $mform->addHelpButton('coursegroups', 'coursegroups', 'report_reflectionexporter');
-        $mform->hideIf('coursegroups', 'onbehalf', 'notchecked');
 
-        // Add the Teachers found based on the groups. Keep them all hidden unless the teacher picks the group this teacher belongs to.
+        // Check that there are groups created.
+        if (count ($grouparray) > 0 ) {
+
+            $select = $mform->addElement('select', 'coursegroups', get_string('coursegroups', 'report_reflectionexporter'), $grouparray);
+            $select->setMultiple(true);
+            $mform->addHelpButton('coursegroups', 'coursegroups', 'report_reflectionexporter');
+            $mform->hideIf('coursegroups', 'onbehalf', 'notchecked');
+        }
+
+        // Add the Teachers found based on the groups.
+        // Keep them all hidden unless the teacher picks the group this teacher belongs to.
         foreach ($grouparrayaux as $aux) {
 
             $details = $aux->teachers;
             foreach ($details as $detail) {
-                $fieldname = "teacher_" . $aux->groupid . '_' . $detail->id; // 
+                $fieldname = "teacher_" . $aux->groupid . '_' . $detail->id;
                 $si = substr($detail->firstname, 0, 1) . '.' . substr($detail->lastname, 0, 1);
-                $label = get_string('teacheringroup', 'report_reflectionexporter', ['firstname' => $detail->firstname, 'lastname' => $detail->lastname,]);
-               
+                $label = get_string('teacheringroup', 'report_reflectionexporter', ['firstname' => $detail->firstname, 'lastname' => $detail->lastname, ]);
+
                 $mform->addElement('text', $fieldname, $label, array('size' => '6', 'class' => 'teacher-initial-field-hide'));
                 $mform->settype($fieldname, PARAM_TEXT);
                 $mform->setDefault($fieldname, $si);
                 $mform->hideIf($fieldname, 'onbehalf', 'notchecked');
-               
+
                 $detail->si = $si;
                 $detail->groupid = $aux->groupid;
                 $detail->students = $aux->students;
@@ -122,21 +132,20 @@ class reflectionexporter_form extends moodleform {
         }
 
         // Keep a JSON to keep track of the groups selected.
-        $mform->addElement('text', 'groupselectionjson', 'Group Selection JSON'/*, ['hidden' => true]*/);
+        $mform->addElement('text', 'groupselectionjson', 'Group Selection JSON');
         $mform->settype('groupselectionjson', PARAM_TEXT);
         $mform->setDefault('groupselectionjson', '[]');
         $mform->disabledIf('groupselectionjson', 'onbehalf', 'notchecked');
         // Auxiliar input to keep a reference to the full list of teachers in the groups. This is used in the JS.
-        $mform->addElement('text', 'groupselectionjsonaux', 'Group Selection JSON AUX'/*, ['hidden' => true]*/);
+        $mform->addElement('text', 'groupselectionjsonaux', 'Group Selection JSON AUX');
         $mform->settype('groupselectionjsonaux', PARAM_TEXT);
         $mform->setDefault('groupselectionjsonaux', json_encode($teachersingroups));
         $mform->disabledIf('groupselectionjsonaux', 'onbehalf', 'notchecked');
 
-
         // Allocated students. Only if the teacher is filling the form on behalf of another teacher, because if that is the case, thenwe ha
         reflectionexportermanager::get_active_users($this->_customdata['id']);
 
-        $students =  reflectionexportermanager::get_active_users($this->_customdata['id']);
+        $students = reflectionexportermanager::get_active_users($this->_customdata['id']);
         $studentsarray = array();
 
         foreach ($students as $uid => $student) {
@@ -211,8 +220,8 @@ class reflectionexporter_form extends moodleform {
     public function validation($data, $files) {
 
         $errors = parent::validation($data, $files);
-       
-        if ($data['assessments']  == 0) {
+
+        if ($data['assessments'] == 0) {
             $errors['assessments'] = get_string('assessmenterror', 'report_reflectionexporter');
         }
         if ($data['assessments2'] == 0) {
@@ -227,7 +236,7 @@ class reflectionexporter_form extends moodleform {
             if (count($data['userid']) == 0) {
                 $errors['userid'] = get_string('useridterror', 'report_reflectionexporter');
             }
-    
+
             if (empty($data['supervisorinitials'])) {
                 $errors['supervisorinitials'] = get_string('supervisorinitialserror', 'report_reflectionexporter');
             }
